f89b3e881d6761be1c0c2f848c19f5d14bdec848
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-10-11 00:52:34 +0700

    ASSR-2 created initial development environment

---
+++ AssrWeb/MetaCommon/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MetacommonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MetaCommon'
---
+++ AssrWeb/UploadSource/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UploadsourceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'UploadSource'
