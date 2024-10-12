b9a600729135b1a9e3c627ebca8ed99188d3c894
CatGamer7 <arthur.zayda@gmail.com>
2024-10-12 23:59:12 +0700

    Update: doc strings for exceptions

--- AssrWeb/MetaCommon/table_operations/IndexListOOBException.py
+++ AssrWeb/MetaCommon/table_operations/IndexListOOBException.py
@@ -1,4 +1,6 @@
 class IndexListOOBException(Exception):
+    """Exception rasied when index int columns of 
+     a table is out of bounds"""
 
     def __init__(self, *args, id_list=[]):
         self.id_list = id_list
