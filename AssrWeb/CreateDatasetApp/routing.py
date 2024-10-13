890b2a91c51825c4e28c97b0ac229754dd3a0709
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-10-13 23:51:27 +0700

    ASSR-10 fixed dataset creation routing.py

--- AssrWeb/CreateDatasetApp/routing.py
+++ AssrWeb/CreateDatasetApp/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from CreateDatasetApp.consumer import CreationConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/socket-download-source/', CreationConsumer.as_asgi())
+    re_path(r'ws/socket-dataset-create/', CreationConsumer.as_asgi())
 ]
