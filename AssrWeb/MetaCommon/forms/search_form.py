05506d0448119588e1d56243f1e0c7cc25436e17
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-11-02 22:30:40 +0700

    test rebase

--- AssrWeb/MetaCommon/forms/search_form.py
+++ AssrWeb/MetaCommon/forms/search_form.py
@@ -4,11 +4,13 @@ from django import forms
 class SearchForm(forms.Form):
     tags = []
     tags_list = [(i.name, i.name) for i in tags]
-    search_query = forms.CharField(widget=forms.TextInput(attrs={"class": "btn btn-main main-text w-100"}),required=False,)
-    tags =  forms.MultipleChoiceField(
+    search_query = forms.CharField(widget=forms.TextInput(attrs={"class": "btn btn-main main-text w-100"}),
+                                   required=False, )
+    tags = forms.MultipleChoiceField(
         required=False,
         widget=forms.CheckboxSelectMultiple,
         choices=tags_list,
     )
+
     class Meta:
         abstract = True
