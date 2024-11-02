7c4e9db7f91fe0e5347236c5f5479d88ed55cc6b
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-11-02 19:29:52 +0700

    Abstract form created

--- AssrWeb/CreateDatasetApp/forms/dataset_search_form.py
+++ AssrWeb/CreateDatasetApp/forms/dataset_search_form.py
@@ -1,12 +1,6 @@
-from django import forms
 from CreateDatasetApp.models import DatasetTags
-
-class DatasetSearchForm(forms.Form):
+from MetaCommon.forms import SearchForm
+class DatasetSearchForm(SearchForm):
     tags = DatasetTags.objects.all()
-    tags_list = [(i.name, i.name) for i in tags]
-    search_query = forms.CharField(widget=forms.TextInput(attrs={"class": "btn btn-main main-text w-100"}),required=False,)
-    tags =  forms.MultipleChoiceField(
-        required=False,
-        widget=forms.CheckboxSelectMultiple,
-        choices=tags_list,
-    )
+    class Meta:
+        abstract = False
