c15d29de555b086b862b56c3d9228f25245de9a1
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-11-02 17:19:01 +0700

    List page created

--- AssrWeb/CreateDatasetApp/views.py
+++ AssrWeb/CreateDatasetApp/views.py
@@ -13,8 +13,6 @@ from UploadSource.models import SourceFile
 from json import loads
 
 # Create your views here.
-
-
 def create_view(request):
     context = {
         "metadataForm": DatasetMetadataForm(),
@@ -120,3 +118,33 @@ def _create_table(pk_list: list[int]) -> TableCreator:
     file_bytes = [file["ancestorFile"] for file in file_objs]
 
     return TableCreator(file_bytes)
+
+
+def show_list(request):
+    search_form = DatasetSearchForm()
+    search_query = request.GET.get('search_query', None)
+    context = {
+        'form': search_form,
+        'page': 'Датасеты',
+        'create_name': "Датасет",
+    }
+    selected_tags = request.GET.getlist('tags')
+    print(search_query, selected_tags)
+    if search_query is None and len(selected_tags) == 0:
+        all_datasets = DatasetMetadata.objects.order_by('name')
+        paginator = Paginator(all_datasets, 8)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['page_obj'] = page_obj
+    else:
+        search_result = DatasetMetadata.objects.filter(Q(name__contains=search_query))
+        if search_query is not None and len(selected_tags) != 0:
+            print("Tags_received")
+            selected_tags = [i for i in DatasetTags.objects.filter(Q(name__in=selected_tags) )]
+            print(selected_tags)
+            search_result = DatasetMetadata.objects.filter(Q(name__contains=search_query) & Q(tag__in=selected_tags))
+        paginator = Paginator(search_result, 8)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['page_obj'] = page_obj
+    return render(request, "Datasets/dataset-list.html", context)
