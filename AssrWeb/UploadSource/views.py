7eefec19e0e830b2b4e4c55c70f55570bf6fc404
No1CareZ <dragonworld15@gmail.com>
2024-11-03 23:33:35 +0700

    (Update) SourceContentCreator
    Minor changes in tag and metadata models, added SourceContentCreator

--- AssrWeb/UploadSource/views.py
+++ AssrWeb/UploadSource/views.py
@@ -2,16 +2,17 @@ from json import loads
 from django.core.paginator import Paginator
 from django.db.models import Q, QuerySet
 from django.http.response import HttpResponse, JsonResponse, HttpResponseBadRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST
 from DjangoAssr.settings import PER_PAGE
-from django.shortcuts import render, get_object_or_404
 from django.db import IntegrityError
 from UploadSource.forms.SourceMetadataForm import SourceMetadataForm
 from UploadSource.models import SourceTags, SourceMetadata, SourceFile
 from UploadSource.file_checker import FileChecker
 import base64
+from .source_content_creator import ContentCreator
+from CreateDatasetApp.table_creator import TableCreator
 
 
 def upload_page_view(request):
@@ -77,8 +78,8 @@ def filter_source_view(request):
     return JsonResponse(response)
 
 
-def _get_paginated_source_files(filter_contains = "",
-                                page_number = 0) -> QuerySet:
+def _get_paginated_source_files(filter_contains="",
+                                page_number=0) -> QuerySet:
 
     files = SourceFile.objects.prefetch_related("metadata")
     if filter_contains:
@@ -102,13 +103,19 @@ def details_page_view(request, metadata_id):
     metadata = get_object_or_404(SourceMetadata, id=metadata_id)
     sourceFile = get_object_or_404(SourceFile, metadata=metadata)
 
-    recived_data = sourceFile.ancestorFile
     file_data = base64.b64encode(sourceFile.ancestorFile).decode()
+
+    creator = ContentCreator([sourceFile.ancestorFile])
+    output = creator.to_html_embed()
+    cr = TableCreator([sourceFile.ancestorFile])
+    test = cr.to_html()
 
     context = {
         "metadata": metadata,
         "sourceFile": sourceFile,
         "file": file_data,
+        "output": output,
+        "test": test,
     }
 
     return render(request, "SourceFiles/details.html", context)
