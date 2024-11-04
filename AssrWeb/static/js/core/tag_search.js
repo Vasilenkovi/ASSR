1fd0b5c8b362096d322b8d46941a9cbce8933fcd
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-11-04 15:28:22 +0700

    Fix: changed tag selection

--- AssrWeb/static/js/core/tag_search.js
+++ AssrWeb/static/js/core/tag_search.js
@@ -1,23 +1,25 @@
 document.querySelector('.searchable-tag').onchange = function() {
     var tags = this.querySelectorAll('.searchable-tag');
-    for(let i  of tags)
+    for(let j of tags)
     {
-        console.log(i.value);
-        console.log(i.checked);
-        if( document.querySelector('#tag-'+i.value) )
+    let i = j.parentElement;
+                console.log("started");
+        if(document.querySelector('#tag-'+i.textContent.trim()) )
         {
+            console.log("exist");
             continue;
         }
-        if(i.checked==false)
+        if(j.checked==false)
         {
+                    console.log("unchecked");
              continue;
         }
         else
         {
              let added_tag = document.createElement('label');
              added_tag.className  = "main-text deletable-tag tag-element";
-             added_tag.id = "tag-"+i.value;
-             added_tag.innerHTML = i.value;
+             added_tag.id = "tag-"+i.textContent.trim();
+             added_tag.innerHTML = i.textContent.trim();
              var search_tags = document.querySelector('.tags-list');
              search_tags.append(added_tag);
         }
