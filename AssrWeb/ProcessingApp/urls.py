1f3d69ca11d59674bf3446402d2b7a00077d8566
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2025-03-05 02:47:32 +0700

    Added: list of processes, process page and retrieving results in json and csv

--- AssrWeb/ProcessingApp/urls.py
+++ AssrWeb/ProcessingApp/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from ProcessingApp.views import AcceptTaskView, launch_task
-
+from ProcessingApp.views import (
+    AcceptTaskView, 
+    launch_task, 
+    processes_list, 
+    task_results, 
+    download_processing_results
+)
 
 app_name = "processing"
 
 urlpatterns = [
     path("new-process/", AcceptTaskView.as_view(), name="new-process"),
-    path("dummy-task/", launch_task, name="dummy-task")
+    path("dummy-task/", launch_task, name="dummy-task"),
+    path("list/", processes_list, name="list"),
+    path("view/<slug:task_pk>/", task_results, name="view"),
+    path("view/<slug:task_pk>/download/", download_processing_results, name="download"),
 ]
--- AssrWeb/ProcessingApp/views/__init__.py
+++ AssrWeb/ProcessingApp/views/__init__.py
@@ -1,1 +1,2 @@
 from .accept_task_view import AcceptTaskView, launch_task
+from .results_display import processes_list, task_results, download_processing_results
