03b128388db553e7faf8753655421a115e99f096
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2025-03-08 23:07:42 +0700

    Merge: fixed imports

--- AssrWeb/ProcessingApp/views/__init__.py
+++ AssrWeb/ProcessingApp/views/__init__.py
@@ -1,7 +1,4 @@
-from .accept_task_view import AcceptTaskView, launch_task
 from .results_display import processes_list, task_results, download_processing_results
 from .accept_task_view import AcceptTaskView
 from .list_result_view import List_Result_View
 from .guide_view import Guide_view
-
-from .results_display import processes_list, task_results, download_processing_results
