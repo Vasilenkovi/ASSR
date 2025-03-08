f2c4b5530eb17ba0f5e45ebf4dd7a4f6a32b5ba4
No1CareZ <dragonworld15@gmail.com>
2025-03-08 17:22:27 +0700

    (UPDATE) changed callajax script

--- AssrWeb/static/js/sourceFile/callajax.js
+++ AssrWeb/static/js/sourceFile/callajax.js
@@ -27,23 +27,30 @@ document.querySelector('#renderMore').onclick = async function getMoreLines() {
         'post',
         JSON.stringify({'last-row': last_row})
     )
-    let tablebody = document.getElementById('table_rows')
+    let tablebody = document.getElementById('table-rows')
 
     let columns = Object.keys(data)
-    for (let i = last_row; i < Object.keys(data[columns[0]]).length + last_row; i++){
-        let insert_tr = document.createElement('tr')
-        insert_tr.insertAdjacentHTML(
-            'beforeend',
-            "<th>" + i + "</th>"
-        )
-        columns.forEach(column => {
-            let something = data[column][i]
-            insert_tr.insertAdjacentHTML(
+    if (Object.keys(data[columns[0]]).length != 0){
+        for (let index_row = last_row; index_row < Object.keys(data[columns[0]]).length + last_row; index_row++){
+            let insert_tr = document.createElement('tr')
+            insert_tr.insertAdjacentHTML( // insert index
                 'beforeend',
-                "<td>" + something + "</td>"
+                `<th data-col="0" data-row="${index_row}">\n<input value="${index_row}"/>\n</th>`
             )
-        });
-        tablebody.append(insert_tr)
+            for (let column_index = 0; column_index < columns.length; column_index++){ // insert each column
+                let contents = data[columns[column_index]][index_row]
+                insert_tr.insertAdjacentHTML(
+                    'beforeend',
+                    `<td data-col="${column_index}" data-row="${index_row}">\n<input value="${contents}"/>\n</td>`
+                )
+            }
+            tablebody.append(insert_tr) // append
+        }
+        last_row += Object.keys(data[columns[0]]).length
     }
-    last_row += Object.keys(data[columns[0]]).length
+    else {
+        let button = document.getElementById('renderMore')
+        button.disabled = true
+        button.innerText = "Got nothing to render :("
+    }
 }
