65c48224697f0318ba72775e15a86ef89d662588
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2025-03-08 23:11:24 +0700

    i am eblan

--- AssrWeb/ProcessingApp/urls.py
+++ AssrWeb/ProcessingApp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from ProcessingApp.views import AcceptTaskView, List_Result_View, Guide_view
 
 from ProcessingApp.views import (
+    List_Result_View,
+    Guide_view,
     AcceptTaskView,
-    launch_task,
     task_results,
     download_processing_results
 )
@@ -12,12 +12,8 @@ app_name = "processing"
 
 urlpatterns = [
     path("new-process/", AcceptTaskView.as_view(), name="new-process"),
-    path("dummy-task/", launch_task, name="dummy-task"),
     path("view/<slug:task_pk>/", task_results, name="view"),
     path("view/<slug:task_pk>/download/", download_processing_results, name="download"),
     path("tasks/", List_Result_View.as_view(), name="task-list"),
     path("guide/", Guide_view.as_view(), name="guide"),
-    path("dummy-task/", launch_task, name="dummy-task"),
-    path("view/<slug:task_pk>/", task_results, name="view"),
-    path("view/<slug:task_pk>/download/", download_processing_results, name="download"),
 ]
--- AssrWeb/ProcessingApp/views/__init__.py
+++ AssrWeb/ProcessingApp/views/__init__.py
@@ -1,4 +1,4 @@
-from .results_display import processes_list, task_results, download_processing_results
+from .results_display import task_results, download_processing_results
 from .accept_task_view import AcceptTaskView
 from .list_result_view import List_Result_View
 from .guide_view import Guide_view
